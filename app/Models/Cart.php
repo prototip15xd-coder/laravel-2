7239f0452be0c7202285475cd7ec5fb74c80a619
Marina R <prototip15xd@gmail.com>
2026-05-01 17:21:49 +0700

    перед 8.9

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -25,7 +25,10 @@ class Cart extends Model
         ];
     }
 
-    public function user() { return $this->belongsTo(User::class); }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function items() { return $this->hasMany(CartItem::class); }
 }
